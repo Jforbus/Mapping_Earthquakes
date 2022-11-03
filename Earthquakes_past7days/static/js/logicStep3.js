55772582eba7e5130117e4a6d1f29496427df036
Jforbus <109490597+Jforbus@users.noreply.github.com>
2022-11-03 15:34:52 -0500

    Style markers based on earthquake magnitude.

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -47,13 +47,13 @@ d3.json(earthquakeData).then(function(data) {
         return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: "#ffae42",
+        fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true,
         weight: 0.5
         };
-    }
+    };
     // This function determines the radius of the earthquake marker based on its magnitude.
     // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
     function getRadius(magnitude) {
@@ -61,12 +61,34 @@ d3.json(earthquakeData).then(function(data) {
             return 1;
         }
         return magnitude * 4;
-    }
-  // Creating a GeoJSON layer with the retrieved data.
+    };
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+        return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+        return "#ea822c";
+        }
+        if (magnitude > 3) {
+        return "#ee9c00";
+        }
+        if (magnitude > 2) {
+        return "#eecc00";
+        }
+        if (magnitude > 1) {
+        return "#d4ee00";
+        }
+        return "#98ee00";
+    };
+
+    // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
         pointToLayer: function(feature, latlng){
-            return L.circleMarker(latlng).bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><h4>Magnitude: " + feature.properties.mag + "</h4>")
-        },
-        style: styleInfo
+            return L.circleMarker(latlng)},
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
     }).addTo(map);
 });
